f890f5fe3df87c4e3f2c4e94e6aae03c8af9d6cd
jlhelwig <jeff.helwig@gmail.com>
2017-04-04 14:20:10 -0500

    show one user

--- routes/users.js
+++ routes/users.js
@@ -2,14 +2,9 @@ var express = require("express");
 	router = express.Router();
 	knex = require("../db/knex")
 
-router.route('/')
-.post(function(req,res){
-  knex('users')
-  console.log('this is home route');
-})
 
-router.route('/')
-	.post(function(req, res){
+    router.route('/')
+	.post(function(req, res) {
 		knex('users')
 			.insert({
 				full_name: req.body.user.full_name,
@@ -17,11 +12,11 @@ router.route('/')
 				img_url: req.body.user.img_url
 			})
 			.returning('id')
-			.then(function(id){
+			.then(function(id) {
 				res.redirect(`/users/${id}`);
 			});
 	})
-	.get(function(req, res) {
+	.get(function(req, res){
 	  knex('users')
 	    .select('id', 'username')
 			.orderBy('id', 'asc')
@@ -33,11 +28,52 @@ router.route('/')
 	      });
 			});
 	});
-router.route('/new')
-    .get((req, res) => {
-        res.render('users/new');
-    });
 
+
+// this is where users will go to add a new user
+router.route("/new")
+	.get((req, res) => {
+	  res.render('users/new');
+	});
+
+// this handles a specific id route
+router.route("/:id")
+	.delete(function(req, res) {
+		knex('users')
+			.del()
+			.where({
+				id: req.params.id
+			})
+			.then(function() {
+				res.redirect(`/users`);
+			});
+	})
+	.get(function(req, res) {
+	  knex('users')
+	    .select('id', 'username', 'full_name', 'img_url')
+	    .where({ id: req.params.id })
+	    .first()
+	    .then(function(user) {
+				// this passes the user to the ejs template
+	      res.render('users/show', {
+	        user
+	      });
+	    });
+	})
+	.put(function(req, res) {
+	  knex('users')
+	    .update({
+	      full_name: req.body.user.full_name,
+	      username: req.body.user.username,
+	      img_url: req.body.user.img_url
+	    })
+	    .where({
+	      id: req.params.id
+	    })
+	    .then(function() {
+	      res.redirect(`/users/${req.params.id}`);
+	    });
+	});
 
 
 
