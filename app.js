718aa83933a0fc524ee7447c47d8f959d36e9c5c
jlhelwig <jeff.helwig@gmail.com>
2017-04-04 11:30:46 -0500

    more files

---
+++ app.js
@@ -0,0 +1,31 @@
+var express = require("express"),
+	app = express(),
+	methodOverride = require("method-override"),
+	morgan = require("morgan"),
+	bodyParser = require("body-parser"),
+	usersRouter = require("./routes/users");
+	postsRouter = require("./routes/posts");
+	// commentsRouter= require("./routes/comments");
+require("locus");
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
+app.use(morgan("tiny"));
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(methodOverride("_method"));
+
+app.get("/", function(req, res){
+	res.render("statics/home")
+});
+
+// Routes
+app.use("/users", usersRouter);
+app.use("/posts", postsRouter);
+// app.use("/comments", commentsRouter);
+
+// app.get("*", function(req,res){
+//   res.render("404");
+// });
+
+app.listen(3000, function(){
+  console.log("Server is listening on port 3000");
+});
